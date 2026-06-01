89c7b593c27e595002c5b8bdcbe26fa198849dc0
Arthur fortuna <arthurfortuna17@gmail.com>
2026-06-01 19:40:52 -0300

    Converte projeto para TypeScript

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/auth/:path*',
+        destination: '/api/auth/:path*',
+      },
+    ]
+  },
+}
+
+export default nextConfig
